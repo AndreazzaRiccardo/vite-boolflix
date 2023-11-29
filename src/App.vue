fb487e2975851c87b5a5465b39d892fcae41f05a
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2023-11-29 16:08:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+ 
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+@use "@fortawesom/fontawesome-free/css/all.css";
+
+</style>
